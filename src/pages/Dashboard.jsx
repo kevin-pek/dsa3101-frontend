7b300c9bdec2fa13bcf36406358ddceb8ef9fdbe
yuxinchan <e0772657@u.nus.edu>
2024-03-24 02:38:41 +0800

    Added line chart for historical hiring costs

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,51 @@
+import React, { useEffect, useRef} from 'react';
+import Chart from 'chart.js/auto';
+
 export function Dashboard() {
-  return <p>Dashboard</p>
+  const chartContainer = useRef(null);
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = chartContainer.current.getContext('2d');
+
+    const hiringExpenditure = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      datasets: [
+        {
+          label: 'Monthly Hiring Expenditure (in Thousands)',
+          data: [10, 12.5, 13, 17],
+          borderColor: 'rgb(47, 173, 102)',
+          borderWidth: 3,
+          fill: true,
+        },
+      ],
+    };
+
+    chartRef.current = new Chart(ctx, {
+      type: 'line',
+      data: hiringExpenditure,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+    return () => {
+      if (chartRef.current) {
+        chartRef.current.destroy(); // Cleanup previous chart instance on component unmount
+      }
+    };
+
+  }, []);
+
+  return (
+    <div style = {{ padding: '20px', textAlign: 'center'}}>
+      <h1>Dashboard</h1>
+      <h3>2024 Monthly Hiring Expenditure</h3>
+      <canvas ref={chartContainer} width="400" height="200"></canvas>
+    </div>
+  );
 }
