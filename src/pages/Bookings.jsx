fb6e075591f9a6541c91f52ccdd405162a854607
fadilahzak <e0774559@u.nus.edu>
2024-03-29 04:07:42 +0800

    updated bookings.jsx with api link

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,78 +1,99 @@
 import React from 'react';
-import { MantineReactTable, useMantineReactTable } from 'mantine-react-table';
-import { ActionIcon, Button, Tooltip } from '@mantine/core';
+import { useState, useMemo } from "react";
+import { MantineReactTable, useMantineReactTable,MRT_EditActionButtons } from 'mantine-react-table';
+import { ActionIcon,
+  Button,
+  Tooltip,
+  FileButton,
+  Text,
+  Group,
+  Flex,
+  Title,
+  Stack, } from '@mantine/core';
 import { IconEdit, IconTrash } from '@tabler/icons-react';
 import { fakeBookings } from '../sampleBookings';
+import { updateBooking } from '../api/booking'
 
 export function Bookings() {
-  const columns = [
-    { 
-      accessorKey: 'eventName',
-      header: 'Event Name', 
-      mantineEditTextInputProps: ({ cell, row }) => ({ 
-        type: 'text',
-        required: true
-      })
-    },
-    {
-      accessorKey: 'eventDate',
-      header: 'Event Date', 
-      mantineEditTextInputProps: ({ cell, row }) => ({ 
-        type: 'date', 
-        required: true
-      })
-    },
-    { // try and get this automated once you get the day
-      accessorKey: 'eventDay',
-      header: 'Event Day', 
-      mantineEditTextInputProps: ({ cell, row }) => ({ 
-        type: 'select', 
-        options: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday', 'Sunday'],
-        required: true
-      })
-    },
-    {
-      accessorKey: 'eventSTime',
-      header: 'Start Time',
-      mantineEditTextInputProps: ({cell,row}) => ({
-        type: 'time',
-        required: true
-      })
-    },
-    {
-      accessorKey: 'eventETime',
-      header: 'End Time',
-      mantineEditTextInputProps: ({cell,row}) => ({
-        type: 'time',
-        required: true
-      })
-    },
-    {
-      accessorKey: 'numPax',
-      header: 'Number of Pax',
-      mantineEditTextInputProps: ({cell,row}) => ({
-        type: 'number',
-        required: true
-      })
-    },
-    {
-      accessorKey: 'staffReq',
-      header: 'Number of Staff Required',
-      mantineEditTextInputProps: ({cell,row}) => ({
-        type: 'number',
-        required: true
-      })
-    },
-    {
-      accessorKey: 'remark',
-      header: 'Remarks (if any)',
-      mantineEditTextInputProps: ({cell,row}) => ({
-        type: 'Text',
-        required: false
-      })
-    }
-  ];
 
+
+  const columns = useMemo(
+    () => [ 
+      { 
+        accessorKey: 'eventName',
+        header: 'Event Name', 
+        mantineEditTextInputProps: ({ cell, row }) => ({ 
+          type: 'text',
+          required: true
+        })
+      },
+      {
+        accessorKey: 'eventDate',
+        header: 'Event Date', 
+        mantineEditTextInputProps: ({ cell, row }) => ({ 
+          type: 'date', 
+          required: true
+        })
+      },
+      { // try and get this automated once you get the day
+        accessorKey: 'eventDay',
+        header: 'Event Day', 
+        mantineEditTextInputProps: ({ cell, row }) => ({ 
+          type: 'select', 
+          options: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday', 'Sunday'],
+          required: true
+        })
+      },
+      {
+        accessorKey: 'eventSTime',
+        header: 'Start Time',
+        mantineEditTextInputProps: ({cell,row}) => ({
+          type: 'time',
+          required: true
+        })
+      },
+      {
+        accessorKey: 'eventETime',
+        header: 'End Time',
+        mantineEditTextInputProps: ({cell,row}) => ({
+          type: 'time',
+          required: true
+        })
+      },
+      {
+        accessorKey: 'numPax',
+        header: 'Number of Pax',
+        mantineEditTextInputProps: ({cell,row}) => ({
+          type: 'number',
+          required: true
+        })
+      },
+      {
+        accessorKey: 'staffReq',
+        header: 'Number of Staff Required',
+        mantineEditTextInputProps: ({cell,row}) => ({
+          type: 'number',
+          required: true
+        })
+      },
+      {
+        accessorKey: 'remark',
+        header: 'Remarks (if any)',
+        mantineEditTextInputProps: ({cell,row}) => ({
+          type: 'Text',
+          required: false
+        })
+      }
+    ],
+    [validationErrors],
+  )
+  
+// UPDATE action
+  const handleSaveBooking = async ({ values, table }) => {
+    await updateBooking(values)
+    setValidationErrors({})
+    table.setEditingRow(null)
+  }
   const table = useMantineReactTable({
     columns,
     data: fakeBookings,
@@ -83,7 +104,7 @@ export function Bookings() {
     positionActionsColumn: 'last',
     enableBottomToolbar: false,
     onEditingRowCancel: () => setValidationErrors({}),
-    onEditingRowSave: handleSaveEmployee,
+    onEditingRowSave: handleSaveBooking,
     renderEditRowModalContent: ({ table, row, internalEditComponents }) => (
       <Stack>
         <Title order={3}>Edit Event</Title>
@@ -93,8 +114,8 @@ export function Bookings() {
         </Flex>
       </Stack>
     ),
-    renderRowActions: ({ row }) => (
-      <>
+    renderRowActions: ({ row,table }) => (
+      <Flex gap="md">
         <Tooltip label="Edit">
           <ActionIcon onClick={() => table.setEditingRow(row)}>
             <IconEdit />
@@ -105,7 +126,7 @@ export function Bookings() {
             <IconTrash />
           </ActionIcon>
         </Tooltip>
-      </>
+      </Flex>
     ),
     renderTopToolbarCustomActions: ({ table }) => (
       <Button>
