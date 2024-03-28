8564c0ba42094d53a98c75f73d300677a48de934
yuxinchan <e0772657@u.nus.edu>
2024-03-28 09:49:46 +0800

    Added sample data for dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,36 +1,9 @@
 import { LineChart } from '@mantine/charts';
 import { BarChart } from '@mantine/charts';
 import { Text } from '@mantine/core';
+import { Employees, monthlyBookings, empAvailability, demandForecast } from '../sampleDashboard.jsx';
 
 export function Dashboard() {
-  const Employees = [
-    { month: 'Jan', Expenditure: 2890, Workers: 139, Hours: 4709 },
-    { month: 'Feb', Expenditure: 2700, Workers: 176, Hours: 1017 },
-    { month: 'Mar', Expenditure: 3600, Workers: 228, Hours: 376 },
-    { month: 'Apr', Expenditure: 2500, Workers: 232, Hours: 6922 },
-    { month: 'May', Expenditure: null, Workers: null, Hours: null },
-    { month: 'Jun', Expenditure: null, Workers: null, Hours: null },
-    { month: 'Jul', Expenditure: null, Workers: null, Hours: null },
-    { month: 'Aug', Expenditure: null, Workers: null, Hours: null },
-    { month: 'Sep', Expenditure: null, Workers: null, Hours: null },
-    { month: 'Oct', Expenditure: null, Workers: null, Hours: null },
-    { month: 'Nov', Expenditure: null, Workers: null, Hours: null },
-    { month: 'Dec', Expenditure: null, Workers: null, Hours: null },
-  ];
-
-  const monthlyBookings = [
-    { month: 'Jan', Bookings: 46 },
-    { month: 'Feb', Bookings: 29 },
-    { month: 'Mar', Bookings: 60 },
-    { month: 'Apr', Bookings: 47 },
-    { month: 'May', Bookings: null },
-    { month: 'Jun', Bookings: null },
-    { month: 'Aug', Bookings: null },
-    { month: 'Sep', Bookings: null },
-    { month: 'Oct', Bookings: null },
-    { month: 'Nov', Bookings: null },
-    { month: 'Dec', Bookings: null },
-  ];
 
   return (
     <>
@@ -41,6 +14,7 @@ export function Dashboard() {
         </Text>
       </div>
 
+      <div style={{ padding: '20px' }}>
       <Text mb="md" pl="md" style={{ fontWeight: 'bold', fontSize: '16px' }}> 
         Monthly Hiring Expenditure:
       </Text>
@@ -49,9 +23,9 @@ export function Dashboard() {
         h={300}
         data={Employees}
         dataKey="month"
-        xAxisLabel="Date"
         yAxisLabel="Expenditure"
         xAxisProps={{ padding: { left: 30, right: 30 } }}
+        xAxisLabel="Date"
         series={[
           { name: "Expenditure", color: 'rgb(47, 173, 102)' },
         ]}
@@ -60,6 +34,9 @@ export function Dashboard() {
         connectNulls="false"
       />
 
+      </div>
+
+      <div style={{ padding: '20px' }} >
       <Text mb="md" pl="md" style={{ fontWeight: 'bold', fontSize: '16px' }}>
         Monthly Bookings:
       </Text>
@@ -77,9 +54,53 @@ export function Dashboard() {
         connectNulls="false"
       />
 
+      </div>
+
+      <div style={{ padding: '20px' }}>
       <Text mb="md" pl="md" style={{ fontWeight: 'bold', fontSize: '16px' }}>
-        Working Hours:
+        Employee Availability:
       </Text>
+
+      <BarChart
+        h={300}
+        data={empAvailability}
+        dataKey="day"
+        type="stacked"
+        xAxisLabel = "Day"
+        orientation="vertical"
+        xAxisProps={{ padding: { left: 30, right: 30 } }}
+        withLegend
+        legendProps={{ layout: 'horizontal', verticalAlign: 'top' }}
+        withTooltip={true}
+        series={[
+          { name: 'PT', color: '#116732' },
+          { name: 'FT', color: '#2FAD66' },
+        ]}
+        tickLine="y"
+      />
+
+      </div>
+      
+
+      <div style={{ padding: '20px' }}>
+      <Text mb="md" pl="md" style={{ fontWeight: 'bold', fontSize: '16px' }}>
+        Customer Demand Forecast:
+      </Text>
+
+      <LineChart
+        h={300}
+        data={demandForecast}
+        dataKey="date"
+        xAxisProps={{ padding: { left: 30, right: 30 } }}
+        series={[
+          { name: 'Customers', color: 'rgb(47, 173, 102)' },
+        ]}
+        curveType="linear"
+        tickLine="xy"
+        connectNulls="false"
+      />
+      
+      </div>
 
     </>
   );
