7a9cc8f383c6136ef148666286a480193d41daa7
yuxinchan <e0772657@u.nus.edu>
2024-04-09 15:35:51 +0800

    Added date picker feature to filter dashboard view by date range

--- src/components/DemandModal.jsx
+++ src/components/DemandModal.jsx
@@ -1,14 +1,41 @@
-import { useDisclosure } from '@mantine/hooks';
-import { Modal, Button } from '@mantine/core';
+import { useDisclosure, useCounter } from '@mantine/hooks';
+import { Modal, Button, Group, Text, Badge } from '@mantine/core';
+import DatePicker from './DatePicker.jsx'
 
 function DemandModal() {
-  const [opened, { open, close }] = useDisclosure(false);
+  const [opened, { close, open }] = useDisclosure(false);
+  const [count, { increment, decrement }] = useCounter(3, { min: 0 });
+
+  const badges = Array(count)
+    .fill(0)
+    .map((_, index) => <Badge key={index}>Badge {index}</Badge>);
 
   return (
     <>
-      <Modal opened={opened} onClose={close} title="Input Figures for Demand Forecasting Below">
-        {/* Modal content */}
-        <p>Insert input parameters here</p>
+    <div>
+      
+    </div>
+      <Modal opened={opened} onClose={close} size="auto" fontWeight="bold" title=" Demand Forecast Figures">
+        <Text>Select Date</Text>
+
+        <div style={{
+          display: 'inline-block',
+          textAlign: 'center',
+          margin: '0 auto',
+        }}>
+          <DatePicker />
+        </div>
+
+        <Text>Input Number of Customers</Text>
+
+        <Group wrap="nowrap" mt="md">
+          {badges}
+        </Group>
+
+        <Group mt="xl">
+          <Button onClick={increment}>Increase</Button>
+          <Button onClick={decrement}>Decrease</Button>
+        </Group>
       </Modal>
 
       <Button onClick={open}>Input Demand Figures</Button>
@@ -16,4 +43,4 @@ function DemandModal() {
   );
 }
 
-export default DemandModal;
+export default DemandModal
