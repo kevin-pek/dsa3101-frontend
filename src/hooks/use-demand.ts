34f63fa6cf88a315d558fbd38ee2fb9afd9411d1
fadilahzak <e0774559@u.nus.edu>
2024-04-26 00:17:07 +0800

    added endpoints to fetch BE data for demand

--- src/hooks/use-demand.ts
+++ src/hooks/use-demand.ts
@@ -1,15 +1,24 @@
 import useSWR, { mutate } from "swr"
 import { fetcher, postRequest } from "../api"
-import { Demand } from "../types/demand"
-import { demand } from "../sampleDashboard"
+import { ActualDemand, Demand, PredictedDemand } from "../types/demand"
 
 export const useDemand = () => {
   const { data, isLoading } = useSWR<Demand[]>("/demand", fetcher)
-
   return { demand: data || [], isLoading }
 }
 
 export const useAddActualDemand = () => {
   return (data: Omit<Demand, "predicted">) =>
     mutate("/demand", async () => await postRequest("/demand", data), false)
+} 
+
+export const usePastDemand = () => {
+  const { data, isLoading } = useSWR<ActualDemand[]>("/get_past_demand", fetcher)
+  return { demand: data || [], isLoading }
 }
+
+export const usePredictedDemand = () => {
+  const { data, isLoading } = useSWR<PredictedDemand[]>("/get_demand_forecast", fetcher)
+  return { demand: data || [], isLoading }
+}
+
